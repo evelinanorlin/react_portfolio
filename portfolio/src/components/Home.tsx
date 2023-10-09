4a0671a54c1e302e66ba8731d47b4ef3b1be8a6a
Evelina Norlin <evelina.norlin@hotmail.com>
2023-10-09 18:35:17 +0200

    added text-animation

--- portfolio/src/components/Home.tsx
+++ portfolio/src/components/Home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
     <section className="home container">
-      <h3>Hi, I'm</h3>
+      <h3 className='italic'>Hi, I'm</h3>
       <h1>Evelina</h1>
       <h4>Freelancing <span className="thin italic">web designer</span> and <span className="thin italic">front end developer</span> student based in Gothenburg, Sweden 💫</h4>
     </section>
