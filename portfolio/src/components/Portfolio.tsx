3911daa24ff1f95ec23a4e1fbbe1836d6ef19c4e
Evelina Norlin <evelina.norlin@hotmail.com>
2023-10-19 12:22:03 +0200

    styling portfolio items

--- portfolio/src/components/Portfolio.tsx
+++ portfolio/src/components/Portfolio.tsx
@@ -1,25 +1,25 @@
-import placeholder from '/portfolio_placeholder.png'
+import { portfolioItems } from '../data/portfolioItems';
 
 export const Portfolio = () => {
+  const itemsHtml = portfolioItems.map((item, index) => {
+    return (
+    <div className='portfolio-item' key={index}>
+      <img src={item.image} alt='portfolio item' />
+        <div className='tech-stack'>
+         {item.techStack.map((tech, index) => {
+            return (
+              <span key={index} className='tech-item gradient-border'>{tech}</span>
+            )
+         })}
+        {item.gitHub ? <a href={item.gitHub} target='_blank' rel='noreferrer' className='rainbow-text'><i className="fa-brands fa-github"></i></a> : null}
+        </div>
+    </div>
+    )
+  })
   return (
     <>
       <section className='portfolio container' id='portfolio'>
-        <div>
-          <img src={placeholder} />
-          <p className='italic'>React, SASS, node.js</p>
-        </div>
-        <div>
-          <img src={placeholder} />
-          <p className='italic'>React, SASS, node.js</p>
-        </div>
-        <div>
-          <img src={placeholder} />
-          <p className='italic'>React, SASS, node.js</p>
-        </div>
-        <div>
-          <img src={placeholder} />
-          <p className='italic'>React, SASS, node.js</p>
-        </div>
+        {itemsHtml}
       </section>
     </>
   )
---
+++ portfolio/src/data/portfolioItems.ts
@@ -0,0 +1,38 @@
+export const portfolioItems = [
+  {
+    title: 'yoga talks',
+    type: 'freelance project',
+    description: 'A website for a yoga teacher',
+    image: '/portfolio_placeholder.png',
+    myWork: ['web design', 'wordpress development', 'graphic profile'],
+    techStack: ['wordpress', 'divi theme'],
+    link: 'https://yogatalks.se'
+  },
+  {
+    title: 'm/s Virgo',
+    type: 'freelance project',
+    description: 'A website for a ship',
+    image: '/portfolio_placeholder.png',
+    myWork: ['web design', 'web development', 'logo design', 'graphic profile'],
+    techStack: ['react.js', 'typescript', 'SASS', 'Adobe Illustrator'],
+    link: 'https://msvirgo.se',
+  },  {
+    title: 'Yrkesvägledarn',
+    type: 'school project',
+    description: 'A website for advise young people about education and job possibilities, in collaboration with Arbetsförmedlingen',
+    image: '/portfolio_placeholder.png',
+    myWork: ['web design', 'web development'],
+    techStack: ['react.js', 'typescript', 'SASS', 'styled components', 'axios', 'Figma'],
+    link: 'https://remarkable-mandazi-92caa0.netlify.app/',
+    gitHub: 'https://github.com/evelinanorlin/af-case-ver-2'
+  },  {
+    title: 'Tic Tac Toe',
+    type: 'school project',
+    description: 'A Tic Tac Toe game',
+    image: '/portfolio_placeholder.png',
+    myWork: ['web design', 'web development'],
+    techStack: ['vue.js', 'typescript', 'SASS'],
+    link: 'https://tictactoe-portfolio.netlify.app/',
+    gitHub: 'https://github.com/evelinanorlin/tic-tac-toe'
+  }
+]
